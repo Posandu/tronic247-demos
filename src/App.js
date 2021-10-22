066a48862f0b96978913340a1e591c5130f95261
Posandu Mapa <76736580+Posandu@users.noreply.github.com>
2021-10-22 21:36:37 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,12 +14,37 @@ function App() {
 
 function Hero() {
   return (
-    <div className="main">
-      <Box sx={{textAlign:"center"}}>
-      <img src="https://www.tronic247.com/favicon.ico" alt=""/>
-      <h1>Tronic247 Demos</h1>
-      <p>Some Tests and Demos</p>
-      <Button size="large" variant="contained">See the Demos</Button>
+    <div className="main ani">
+      <Box sx={{ textAlign: "center" }}>
+        <img
+          src="https://www.tronic247.com/favicon.ico"
+          style={{ transitionDelay: "1s" }}
+          alt="Tronic247 logo"
+        />
+
+        <h1
+          className="ani"
+          style={{ transitionDelay: "1.2s" }}
+        >
+          Tronic247 Demos
+        </h1>
+
+
+        <p
+          className="ani"
+          style={{ transitionDelay: "1.4s" }}
+        >
+          Some Tests and Demos
+        </p>
+
+
+        <Button
+          size="large"
+          style={{ transitionDelay: "1.6s" }}
+          variant="contained"
+        >
+          See the Demos
+        </Button>
       </Box>
     </div>
   );
