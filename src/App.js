a40a6cdedf1582710df908d1f9a9da4c587dbf1b
Posandu Mapa <76736580+Posandu@users.noreply.github.com>
2021-10-22 15:38:41 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,8 @@
-import React, { useState, useEffect, useRef } from "react";
+import React from "react";
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
 import "./App.css";
-import Stack from "@mui/material/Stack";
-import Button from "@mui/material/Button";
-import ButtonGroup from "@mui/material/ButtonGroup";
-import Dialog from "@mui/material/Dialog";
-import Editor, { DiffEditor, useMonaco, loader } from "@monaco-editor/react";
-import Grow from "@mui/material/Grow";
-import Paper from "@mui/material/Paper";
-import Tabs from "@mui/material/Tabs";
-import Tab from "@mui/material/Tab";
-import Box from "@mui/material/Box";
-import CircularProgress from "@mui/material/CircularProgress";
-import Card from "@mui/material/Card";
-import CardActionArea from "@mui/material/CardActionArea";
-import CardMedia from "@mui/material/CardMedia";
-import Alert from "@mui/material/Alert";
-import CardContent from "@mui/material/CardContent";
-import Typography from "@mui/material/Typography";
-import TextField from "@mui/material/TextField";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import APIURL from "./config.js";
-import hero from "./App.css";
+
 
 function App() {
   return (
@@ -31,71 +13,14 @@ function App() {
 }
 
 function Hero() {
-  const [name, setrealname] = useState("Stranger");
-  const [dialog, setdialog] = useState(false);
-  const [inputvalue, setinputvalue] = useState("");
-
-  const close = () => {
-    setdialog(false);
-  };
-
-  const open = () => {
-    setdialog(true);
-  };
-
-  const setusername = () => {
-    if (inputvalue) {
-      setrealname(inputvalue);
-    } else {
-      setrealname("Stranger");
-    }
-    setdialog(false);
-  };
-
-  const setrealinputvalue = (e) => {
-    setinputvalue(e.target.value);
-  };
-
-  const editorRef = useRef(null);
-
-  function handleEditorDidMount(editor, monaco) {
-    editorRef.current = editor;
-  }
-
-  function showValue() {
-    alert(editorRef.current.getValue());
-  }
-
   return (
-    <div className="hero">
-      <Editor
-        height="90vh"
-        width="50%"
-        defaultLanguage="javascript"
-        theme="vs-dark"
-        defaultValue="// some comment"
-        onMount={handleEditorDidMount}
-      />
-      <Box sx={{ marginLeft: "30px" }}>
-        <Typography variant="h2">Hello {name}</Typography>
-        <Button size="large" variant="outlined" onClick={open}>
-          I have a name
-        </Button>
+    <div className="main">
+      <Box sx={{textAlign:"center"}}>
+      <img src="https://www.tronic247.com/favicon.ico" alt=""/>
+      <h1>Tronic247 Demos</h1>
+      <p>Some Tests and Demos</p>
+      <Button size="large" variant="contained">See the Demos</Button>
       </Box>
-      <Dialog open={dialog} onClose={close}>
-        <Box sx={{ padding: "20px" }}>
-          <TextField
-            label="Name"
-            onChange={setrealinputvalue}
-            variant="standard"
-          />
-          <br />
-          <br />
-          <Button variant="outlined" onClick={setusername}>
-            Set My Name
-          </Button>
-        </Box>
-      </Dialog>
     </div>
   );
 }
